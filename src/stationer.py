4ea16b19c473e229256582e36f59ebd38c9ab077
somrar <kun.song@student.forsbergsskola.se>
2026-08-27 11:01:37 +0200

    dela till två funktioner

--- src/stationer.py
+++ src/stationer.py
@@ -1,31 +1,26 @@
 import requests
 
 
-def sok_hallplats(namn):
-    url = "https://transport.integration.sl.se/v1/sites?expand=true"
-
+def hamta_hallplatser():
+    url = "https://transport.integration.sl.se/v1/sites"
     response = requests.get(url, timeout=10)
     response.raise_for_status()
+    return response.json()
 
-    sites = response.json()
+def sok_hallplats(namn, hallplatser=None):
+    if hallplatser is None:
+        hallplatser = hamta_hallplatser()
 
     traffar = []
-
-    for site in sites:
+    for site in hallplatser:
         if namn.lower() in site["name"].lower():
-            traffar.append(
-                {
-                    "id": site["id"],
-                    "name": site["name"],
-                }
-            )
-    
+            traffar.append({"id": site["id"], "name": site["name"]})
     return traffar
 
 
 #test
 if __name__ == "__main__":
     results = sok_hallplats("slussen")
-    print(f"{len(results)} has found")
+    print(f"{len(results)} result has found")
     for result in results:
         print(result)
