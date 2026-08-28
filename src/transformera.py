7e8011690eca8b853318f77530bfac32b433a7b2
Nibir <nivir46@gmail.com>
2026-08-28 17:48:10 +0200

    added a direct field((riktning, destination, avgångstid)

--- src/transformera.py
+++ src/transformera.py
@@ -1,5 +1,11 @@
 def platta_ut_avgangar(svar):
     resultat = []
     for x in svar["departures"]:
-        pass
+        avgang = {
+            "riktning_kod": x["direction_code"],
+            "riktning": x["direction"],
+            "destination": x["destination"],
+            "avgar_klocka": x["expected"][11:16],
+        }
+        resultat.append(avgang)
     return resultat
