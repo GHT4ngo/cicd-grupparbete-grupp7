6a14252a969f0a35b7d39602167ae2125f4c33e5
Nibir <nivir46@gmail.com>
2026-08-30 17:24:17 +0200

    fetched and transformed avgang in main

--- src/main.py
+++ src/main.py
@@ -6,7 +6,13 @@ from src.transformera import platta_ut_avgangar
 from src.validera import validera_post
 
 def main():
-    pass
+    config = las_config()
+    
+    svar = hamta_avgangar() 
+    avgangar = platta_ut_avgangar(svar)
+    
+    print(f"Antal hämtade avgångar: {len(avgangar)}")
+    
 
 if __name__ == "__main__":
     main()
