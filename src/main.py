0897edf6d1dca5126d1fed0ece5092ecfba8a729
Nibir <nivir46@gmail.com>
2026-08-30 17:19:55 +0200

    created main.py with imports and a inital structure

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from src.config import las_config
+from src.hamta import hamta_avgangar
+from src.resultat import skriv_resultat
+from src.transformera import platta_ut_avgangar
+from src.validera import validera_post
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
