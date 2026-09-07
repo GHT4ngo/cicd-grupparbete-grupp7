7d2d8d0021c18c4097b156bb930c6e92795768d6
Haydslife <rufus@thedays.se>
2026-09-07 15:18:53 +0200

    add function for accessing dictionary with default value

--- src/linjer.py
+++ src/linjer.py
@@ -1,5 +1,10 @@
-linjefarger = {
+_LINJEFARGER = {
     "Tunnelbanans gröna linje": "#148541",
     "Tunnelbanans röda linje": "#d71d24",
     "Tunnelbanans blå linje": "#007db8",
 }
+
+STANDARDFARG = "#9aa4b2"
+
+def hamta_farg(linjegrupp):
+    return _LINJEFARGER.get(linjegrupp, STANDARDFARG)
